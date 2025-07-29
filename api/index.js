3ffcc7e253a6face13f2aa8f432f39c29d0ff634
Parikshit <parikshit.r2005@gmail.com>
2025-07-29 23:46:18 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -18,6 +18,8 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.options('*', cors());  
+
 // using dependencies
 const express = require("express");
 const app = express();
